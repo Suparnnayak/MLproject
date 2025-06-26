64b4de911a2bfcc2b5980a81fa95e91f9e8cd273
unknown <suparnnayak56@gmail.com>
2025-06-26 13:49:08 +0530

    data_ingestion

---
+++ notebook/test_logger.py
@@ -0,0 +1,6 @@
+# test_logger.py (fixed)
+from src.logger import logging
+
+logging.info("Logger is working!")
+logging.warning("This is a warning.")
+logging.error("This is an error.")
--- src/logger.py
+++ src/logger.py
@@ -2,15 +2,17 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
 
+# Custom file handler with UTF-8 encoding
+file_handler = logging.FileHandler(LOG_FILE_PATH, encoding="utf-8")
+formatter = logging.Formatter("[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[%(asctime)s]%(lineno)d%(name)s-%(levelname)s-%(message)s",
-    level=logging.INFO
-)
- 
+# Set up root logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(file_handler)
